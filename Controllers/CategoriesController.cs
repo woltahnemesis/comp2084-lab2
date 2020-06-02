4a409c3e5ebbfebb0319a307b3ce3688da60ce5f
Rey Walter Magbanua <alucardmagbanua@gmail.com>
2020-06-02 14:53:31 -0400

    Finished lab 2

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Details method that takes the URL parameter
+        public IActionResult Details(string category)
+        {
+            //ViewBag holds the temporary value
+            ViewBag.category = category;
+            return View();
+        }
+    }
+}
